a5f162c1cf7685c3a19e389dab9c5472af530110
Jeremy Dumais <jeremydumais@hotmail.com>
2023-01-08 21:28:56 -0500

    Completed the AddNode method and started the GetNode method.

--- Linked List/singly_linked_list.go
+++ Linked List/singly_linked_list.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 // Node This represent a.
 type Node struct {
 	next  *Node
@@ -8,7 +10,8 @@ type Node struct {
 
 type SingleLinkedList interface {
     Head() *Node
-    AddItem(value string)
+    GetNode(index int) (*Node, error)
+    AddNode(value string)
 }
 
 // Singly linked list: a type of linked list in which each node has only one 
@@ -26,10 +29,24 @@ func (list *singleLinkedList) Head() *Node {
     return list.head
 }
 
-func (list *singleLinkedList) AddItem(value string) {
+func (list *singleLinkedList) GetNode(index int) (*Node, error) {
+    if index < 0 {
+        return nil, errors.New("Index out of range") 
+    }
+    return nil, nil
+}
+
+func (list *singleLinkedList) AddNode(value string) {
     if list.head == nil {
         newNode := &Node{nil, value}
         list.head = newNode
+    } else {
+        // Go to last node
+        var lastNode *Node = list.head
+        for (lastNode.next != nil) {
+            lastNode = lastNode.next
+        }
+        newNode := &Node{nil, value}
+        lastNode.next = newNode
     }
 }
-
--- Linked List/singly_linked_list_test.go
+++ Linked List/singly_linked_list_test.go
@@ -10,11 +10,45 @@ func TestSinglyLinkedList_Make_ReturnNilHead(t *testing.T) {
     assert.Nil(t, actual.Head())
 }
 
+func TestSinglyLinkedList_GetNode_WithIndexMinus1OnEmptyList_ReturnError(t *testing.T) {
+    var actual = MakeSingleLinkedList()
+    res, err := actual.GetNode(-1)
+    assert.Nil(t, res)
+    assert.Equal(t, "Index out of range", err.Error())
+}
+
+//TODO to continue
+
 func TestSinglyLinkedList_AddNode_ReturnNonNilHeadAndOneNode(t *testing.T) {
     var actual = MakeSingleLinkedList()
-    actual.AddItem("Test")
+    actual.AddNode("Test")
     assert.NotNil(t, actual.Head())
     assert.Equal(t, "Test", actual.Head().value)
     assert.Nil(t, actual.Head().next)
+}
+
+func TestSinglyLinkedList_AddTwoNodes_ReturnSuccess(t *testing.T) {
+    var actual = MakeSingleLinkedList()
+    actual.AddNode("Test")
+    actual.AddNode("Test2")
+    assert.NotNil(t, actual.Head())
+    assert.NotNil(t, actual.Head().next)
+    assert.Equal(t, "Test", actual.Head().value)
+    assert.Equal(t, "Test2", actual.Head().next.value)
+    assert.Nil(t, actual.Head().next.next)
+}
+
+func TestSinglyLinkedList_AddThreeNodes_ReturnSuccess(t *testing.T) {
+    var actual = MakeSingleLinkedList()
+    actual.AddNode("Test")
+    actual.AddNode("Test2")
+    actual.AddNode("Test3")
+    assert.NotNil(t, actual.Head())
+    assert.NotNil(t, actual.Head().next)
+    assert.NotNil(t, actual.Head().next.next)
+    assert.Equal(t, "Test", actual.Head().value)
+    assert.Equal(t, "Test2", actual.Head().next.value)
+    assert.Equal(t, "Test3", actual.Head().next.next.value)
+    assert.Nil(t, actual.Head().next.next.next)
 }
 
