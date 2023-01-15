a02d133ab26ea717622747f55fd5e96defe6979f
Jeremy Dumais <jeremydumais@hotmail.com>
2023-01-15 08:53:54 -0500

    Prepare the structure for the double linked list.

---
+++ Linked List/doublylinkedlist/doubly_linked_list_test.go
@@ -0,0 +1,16 @@
+package doublylinkedlist
+
+// Node This represent a node item in the list.
+type Node struct {
+    Prev *Node
+	Next *Node
+	Value string
+}
+
+type DoubleLinkedList interface {
+    
+}
+
+type doublelinkedlist struct {
+
+}
